410128462c29cb89633f6848d2d92988ad4fe35b
jkobza <jonas.r.kobza@vanderbilt.edu>
2025-04-17 00:55:35 -0500

    pure pursuit

--- DriveGoodAVStack/src/planner/pure_pursuit.jl
+++ DriveGoodAVStack/src/planner/pure_pursuit.jl
@@ -1,28 +1,33 @@
-function compute_control(path::Vector{SVector{2,Float64}}, pos::SVector{2,Float64}, heading::Float64;
-        lookahead=5.0, speed=2.0, wheelbase=2.5)
+# function compute_control(path::Vector{SVector{2,Float64}}, pos::SVector{2,Float64}, heading::Float64;
+#         lookahead=5.0, speed=2.0, wheelbase=2.5)
 
-    direction = SVector(cos(heading), sin(heading))
-    goal = find_lookahead_point(path, pos, lookahead, direction)
+#     direction = SVector(cos(heading), sin(heading))
+#     goal = find_lookahead_point(path, pos, lookahead, direction)
 
-    if goal === nothing
-        return (0.0, 0.0, false)
-    end
+#     if goal === nothing
+#         return (0.0, 0.0, false)
+#     end
 
-    rel = goal - pos
-    x_local = dot(rel, direction)
-    y_local = cross(direction, rel)
+#     rel = goal - pos
+#     x_local = dot(rel, direction)
+#     y_local = cross(direction, rel)
 
-    if x_local ≤ 0
-        return (0.0, 0.0, false)
-    end
+#     if x_local ≤ 0
+#         return (0.0, 0.0, false)
+#     end
 
-    curvature = (2 * y_local) / (lookahead^2)
-    steering = atan(wheelbase * curvature)
-    return (steering, speed, true)
+#     curvature = (2 * y_local) / (lookahead^2)
+#     steering = atan(wheelbase * curvature)
+#     return (steering, speed, true)
+# end
+include("../localization.jl")
+function driver(pl::Polyline, loc::LocalizationType)
+    angle = calculate_angle(pl, [loc.lat, loc.long], loc.yaw)
+    return angle
 end
 
 function find_lookahead_point(pl::Polyline,
-                              pos::SVector{2,Float64},
+                              pos::Vector{2,Float64},
                               lookahead::Float64)
     best_pt = nothing
     best_prog = -Inf
@@ -59,7 +64,7 @@ end
 wrap_angle(θ) = mod(θ + π, 2π) - π
 
 function calculate_angle(pl::Polyline,
-    pos::SVector{2,Float64},
+    pos::Vector{2,Float64},
     heading::Float64;
     lookahead=5.0,
     speed=2.0)
